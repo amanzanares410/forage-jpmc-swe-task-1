80dd21097f9dbd447983c529475ac9e68ab38a2f
Ashley Manzanares <fnv2vx@virginia.edu>
2023-07-26 16:23:20 -0400

    updating getData, getRatio, and tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,11 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    expected1 = ('ABC', 120.48, 121.2, (120.48 + 121.2)/2)
+    self.assertTupleEqual(getDataPoint(quotes[0]), expected1)
+
+    expected2 = ('DEF', 117.87, 121.68, (117.87 + 121.68)/2)
+    self.assertTupleEqual(getDataPoint(quotes[1]), expected2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +21,21 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    expected1 = ('ABC', 120.48, 119.2, (120.48 + 119.2)/2)
+    self.assertTupleEqual(getDataPoint(quotes[0]), expected1)
 
+    expected2 = ('DEF', 117.87, 121.68, (117.87 + 121.68)/2)
+    self.assertTupleEqual(getDataPoint(quotes[1]), expected2)
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_regular(self):
+    self.assertEqual(getRatio(15.0, 2.5), 6)
+
+  def test_getRatio_priceAZero(self):
+    self.assertEqual(getRatio(0, 2.5), 0)
+
+  def test_getRation_priceBZero(self):
+    self.assertIsNone(getRatio(15, 0))
 
 
 
